d23289ebf1f852d2ab6e9fdc5570ad41e2dd216e
TomasBozzano <tomycas10@gmail.com>
2024-10-04 19:56:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Game from "./components/Game"
+function App() {
+
+  return (
+    <main className="board">
+      <h1>TA- TE - TI</h1>
+        <Game />
+    </main>
+  )
+}
+
+export default App
